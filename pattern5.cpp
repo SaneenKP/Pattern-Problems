ae083b2c382c4c328dd0b4b00c5aab0c235beaea
SaneenKP <saneenkp2000@gmail.com>
2022-09-05 18:20:21 +0530

    pattern5 implemented.

---
+++ pattern5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+void drawPattern(int height , int st){
+
+    for (int i = 1; i <= height; ++i)
+    {
+        for (int j = 1; j <= i; ++j)
+        {
+            std::cout<<j;
+        }
+        std::cout<<std::endl;
+    }
+}
+
+int main(){
+    int height = 0;
+    int st;
+    std::cout<<"Enter height of the triangle : ";
+    std::cin>>height;
+    std::cout<<"Enter starting number : ";
+    std::cin>>st;
+    drawPattern(height , st);
+}
